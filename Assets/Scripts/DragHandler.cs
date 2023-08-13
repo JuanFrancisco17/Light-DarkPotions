a0f6ffa611d88b1d466bc3fa8331d39708693cac
JuanFrancisco17 <91538010+JuanFrancisco17@users.noreply.github.com>
2023-08-13 18:37:00 +0200

    Initial Item move

---
+++ Assets/Scripts/DragHandler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragHandler : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler
+{
+    [SerializeField] Canvas canvas;
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>(); 
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        
+    }
+}
